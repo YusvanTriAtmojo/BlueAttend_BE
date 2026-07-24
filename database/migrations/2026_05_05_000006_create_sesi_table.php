6faca1143831594ca26497ba1cd1b674a439277e
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2026-07-24 19:12:00 +0700

    Migration tabel sesi dan presensi

---
+++ database/migrations/2026_05_05_000006_create_sesi_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::create('sesi', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_event')->nullable();;
+            $table->foreign('id_event')->references('id')->on('event')->onDelete('cascade');
+            $table->unsignedBigInteger('id_uuid')->nullable();;
+            $table->foreign('id_uuid')->references('id')->on('uuid')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sesi');
+    }
+};
