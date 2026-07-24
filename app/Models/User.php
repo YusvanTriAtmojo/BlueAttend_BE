9f28fdccaeb0fc0957d00fbbfcb935b923e4ad05
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2026-07-24 19:31:29 +0700

    Membuat model data tabel role dan users

---
+++ app/Models/User.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use App\Models\Role; 
+
+class User extends Authenticatable implements JWTSubject
+{
+    use HasFactory, Notifiable;
+
+    public $timestamps = false; 
+    protected $fillable = [
+        'id_role',
+        'nip',
+        'nama',
+        'notlp',
+        'alamat',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class,'id_role');
+    }
+
+
+
+    /**
+     * Untuk JWT (login dengan token)
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [
+            'role' => $this->role->nama_role
+        ];
+    }
+}
