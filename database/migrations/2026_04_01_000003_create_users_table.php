f1c470fb01da923fb79da99284ee11f27a0a8247
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2026-07-24 18:51:00 +0700

    Migration tabel role dan users

---
+++ database/migrations/2026_04_01_000003_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_role')->nullable();
+            $table->foreign('id_role')->references('id')->on('role')->onDelete('set null');
+            $table->string('nama', 50);
+            $table->string('nip', 18)->unique();
+            $table->string('email')->unique();
+            $table->string('alamat', 100);
+            $table->string('notlp', 13);
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('sessions');
+    }
+};
