c2696775ad482921774a2a164037a4a49aa23648
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2026-09-02 19:15:41 +0700

    Menambahkan route CRUD event dan peserta, serta update wajah

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PresensiController;
 use App\Http\Controllers\BleController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\EventController;
 use App\Models\Role;
 
@@ -15,6 +16,7 @@ Route::middleware('auth:api', 'role:peserta')->group(function () {
     Route::post('/presensi', [PresensiController::class, 'store']);
     Route::get('/presensi/riwayat', [PresensiController::class, 'riwayatPresensi']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/face/register', [UserController::class, 'updateFace']);
 });
 
 Route::middleware('auth:api', 'role:admin')->group(function () {
@@ -23,8 +25,12 @@ Route::middleware('auth:api', 'role:admin')->group(function () {
     Route::get('/ble', [BleController::class, 'index']);
     Route::put('/ble/{id}', [BleController::class, 'update']);
     Route::delete('/ble/{id}', [BleController::class, 'destroy']);
-    Route::post('/sesi', [EventController::class, 'store']);
-    Route::get('/sesi', [EventController::class, 'index']);
-    Route::put('/sesi/{id}', [EventController::class, 'update']);
-    Route::delete('/sesi/{id}', [EventController::class, 'destroy']);
+    Route::post('/event', [EventController::class, 'store']);
+    Route::get('/event', [EventController::class, 'index']);
+    Route::put('/event/{id}', [EventController::class, 'update']);
+    Route::delete('/event/{id}', [EventController::class, 'destroy']);
+    Route::post('/peserta', [UserController::class, 'store']);
+    Route::get('/peserta', [UserController::class, 'index']);
+    Route::put('/peserta/{id}', [UserController::class, 'update']);
+    Route::delete('/peserta/{id}', [UserController::class, 'destroy']);
 });
